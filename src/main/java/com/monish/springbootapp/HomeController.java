3d0fa0592219a321d1248b797cf44e6afb3735cc
MONISH <165757154+8monish@users.noreply.github.com>
2026-09-16 05:14:04 -0700

    Folders upload

---
+++ src/main/java/com/monish/springbootapp/HomeController.java
@@ -0,0 +1,44 @@
+package com.monish.springbootapp;
+
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping("")
+    public String get() {
+        return "Hello World";
+    }
+
+    @PostMapping("create")
+    public String create(@RequestBody String payload) {
+        return payload + "Successfully created";
+    }
+
+    @PutMapping("put")
+    public String put(@RequestBody String entity) {
+        return entity+"Successfull put";
+    }
+
+    @DeleteMapping("del/{id}")
+    public String del(@PathVariable String id) {
+      return "Deleted" + " " + id;
+    }
+
+    @GetMapping("getmap")
+    public String getmap(@PathVariable String id) {
+        return id + "-_-";
+    }
+    
+}
