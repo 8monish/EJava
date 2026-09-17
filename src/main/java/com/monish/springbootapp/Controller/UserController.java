bc1747842435b0f0ab0d2e6166559d0e9025b0b6
8monish <8monish@gmail.com>
2026-09-17 12:41:08 +0530

    Added JPA and MariaDB dependencies; configure datasource properties

--- src/main/java/com/monish/springbootapp/Controller/UserController.java
+++ src/main/java/com/monish/springbootapp/Controller/UserController.java
@@ -1,5 +1,7 @@
 package com.monish.springbootapp.Controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.monish.springbootapp.User;
 
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     @GetMapping("/{userId}")
     public User getById(@PathVariable String userId) {
         return new User(userId, "Monish", "jade", "monish.jade@example.com");
+    }
+
+    @GetMapping("/all")
+    public List<User> getAll() {
+        List<User> users = List.of(
+            new User("1", "Monish", "jade", "monish.jade@example.com"),
+            new User("2", "John", "Doe", "john.doe@example.com")
+        );
+        return users;
     }
 }
