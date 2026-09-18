c5116a8c15b1991356532087022e2446a1c10638
8monish <8monish@gmail.com>
2026-09-18 09:34:55 +0530

    Added new endpoints and request definitions for user management in UserController

--- src/main/java/com/monish/springbootapp/Controller/UserController.java
+++ src/main/java/com/monish/springbootapp/Controller/UserController.java
@@ -2,17 +2,18 @@ package com.monish.springbootapp.Controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.monish.springbootapp.User;
 import com.monish.springbootapp.repository.UserRepository;
+
 
 
 
@@ -35,10 +36,16 @@ public class UserController {
         return this.userRepository.findAll();
     }
 
-    @PostMapping("/postcheck")
+    @PostMapping("/adduser")
     public User postcheck(@RequestBody User entity) {
         return this.userRepository.save(entity);
     }
+
+    @GetMapping("/us/{userString}")
+    public User getMethodName(@PathVariable String userString) {
+        return this.userRepository.findById(userString).orElseThrow();
+    }
+    
 
     @PutMapping("/update/{userId}")
     public User update(@PathVariable String userId, @RequestBody User entity) {
